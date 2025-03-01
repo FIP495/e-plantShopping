d1bb62cbc6a7a43151cb259c2308dc67e159a70b
fip <fip@fip.com>
2025-03-01 17:30:53 -0500

    added coming soon to checkout button

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -22,7 +22,7 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleCheckoutShopping = (e) => {
-    alert('Functionality to be added for future reference');
+    alert('Coming soon...');
   };
 
   const handleIncrement = (item) => {
@@ -73,7 +73,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
